1d2531f926264489a6edb81c17675280d108de44
F-Sampaio <f.s1488@outlook.com>
2019-09-05 23:24:39 -0400

    switch added

---
+++ js/switch.js
@@ -0,0 +1,9 @@
+function checked(){
+    var eleverSwitch = document.getElementById('switchDL');
+    if (eleverSwitch.checked = true) {
+        console.log('test dark mode');
+    }
+}
+
+
+document.addEventListener('click', checked, false);
